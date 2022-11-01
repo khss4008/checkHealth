89548dbe3b4e3d363d1e03265cce1226fb730733
kimyoungjin <khss4008@gmail.com>
2022-11-01 17:54:44 +0900

    주석

--- testCodef/src/main/java/com/example/service/CheckService.java
+++ testCodef/src/main/java/com/example/service/CheckService.java
@@ -16,6 +16,11 @@ public class CheckService {
 		super();
 		this.checkRepo = checkRepo;
 	}
+	//supplementName = 영양제 이름, price = 가격, brand = 브랜드, image = 경로, ingredients = 전성분
+	// bioavailability = 표에서 나온 값을 평균한 점수, laxative = 이것도 마찬가지, kidneyDisease = 이것도 마찬가지
+	// consumerLabscore = 컨슈머랩 점수, additionalEfficacy = 부가효과(String에 붙이는 식으로), note = 메모
+	// amount = 함량, requiredCount = 하루 섭취량, formula = 제형, views -> like, sustainedRelease = 서방형
+	
 	
 	@Transactional
 	public void insertSupplement() throws Exception {
